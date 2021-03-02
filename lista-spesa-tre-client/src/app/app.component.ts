517246b4decd759e1b4622b3ada04361defc0d98
Andrea Tangucci <andrea.tangucci@gmail.com>
2021-03-02 10:37:25 +0100

    commit client

--- lista-spesa-tre-client/src/app/app.component.ts
+++ lista-spesa-tre-client/src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Dto } from './dto';
+import { DtoLista } from './dto-lista';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lista-spesa-tre-client';
+  prodotto: string;
+  lista: string[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  add(){
+    let dto: Dto = new Dto();
+    dto.prodotto = this.prodotto;
+    let oss: Observable<DtoLista> = this.http.post<DtoLista>('http://localhost:8080/add', dto);
+    oss.subscribe(d => this.lista = d.lista);
+  }
+
+  removeAll(){
+    let oss: Observable<DtoLista> = this.http.get<DtoLista>('http://localhost:8080/removeAll');
+    oss.subscribe(d => this.lista = d.lista);
+  }
 }
---
+++ lista-spesa-tre-client/src/app/dto-lista.ts
@@ -0,0 +1,5 @@
+import { Dto } from "./dto";
+
+export class DtoLista{
+    lista: string[] = [];
+}
